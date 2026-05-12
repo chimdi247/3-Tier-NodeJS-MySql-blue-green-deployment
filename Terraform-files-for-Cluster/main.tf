e9f05347185d60d5413f2f155c1e80391046bc68
chimdi247 <Chimdi247@gmail.com>
2026-05-12 19:40:26 +0300

    Update main.tf

--- Terraform-files-for-Cluster/main.tf
+++ Terraform-files-for-Cluster/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "eu-west-2"
 }
 
 resource "aws_vpc" "devopsshack_vpc" {
@@ -14,7 +14,7 @@ resource "aws_subnet" "devopsshack_subnet" {
   count = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  availability_zone       = element(["eu-west-2a", "eu-west-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -103,12 +103,12 @@ resource "aws_eks_node_group" "devopsshack" {
   subnet_ids      = aws_subnet.devopsshack_subnet[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 4
+    max_size     = 6
+    min_size     = 4
   }
 
-  instance_types = ["t2.medium"]
+  instance_types = ["m7i-flex.large"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
